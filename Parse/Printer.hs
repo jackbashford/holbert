83f0e85fdd965216e7de7f2f12c2c05a3623371e
Jack Bashford <jack@jackbashford.com>
2025-02-23 17:29:14 +1100

    feat: Begin the printer, HTML-like syntax

--- Parse/Printer.hs
+++ Parse/Printer.hs
@@ -1,14 +1,38 @@
+{-# LANGUAGE OverloadedStrings, LambdaCase #-}
+
 module Parse.Printer where
 
+import Text.Earley.Mixfix as EPM
+import qualified Miso.String as MS
 import qualified Editor as E
 import qualified Item as I
+import qualified SyntaxDecl as SD
 import qualified Prop as P
 import qualified ProofTree as PT
 import qualified Terms as T
 import qualified Rule as R
 import qualified Heading as H
 import qualified Paragraph as PG
-import Data.JSString(JSString)
+import Data.JSString(JSString, pack)
 
 print :: E.Document -> IO JSString
-print = undefined
+print doc = return $ MS.intercalate "\n\n" (map printItem doc)
+
+printItem :: I.Item -> MS.MisoString
+printItem = \case
+  (I.Heading (H.Heading level body)) -> "<H" <> MS.ms level <> "> " <> body <> "</H" <> MS.ms level <> ">"
+  (I.Paragraph (PG.Paragraph body)) -> "<P> " <> MS.ms body <> "</P>"
+  (I.SyntaxDecl (SD.SyntaxDecl items)) -> "<S>\n" <> showSyntax items <> "\n</S>"
+  (I.Rule {}) -> "<R></R>"
+
+showSyntax :: [(Int, MS.MisoString, EPM.Associativity)] -> MS.MisoString
+showSyntax = (MS.intercalate "\n") . map showDecl
+  where
+    showDecl :: (Int, MS.MisoString, EPM.Associativity) -> MS.MisoString
+    showDecl (p, s, a) = "'" <> s <> "' (precedence " <> (MS.ms p) <> ", " <> assoc' <> " associativity)"
+      where
+        assoc' :: MS.MisoString
+        assoc' = case a of
+          LeftAssoc -> "left"
+          RightAssoc -> "right"
+          NonAssoc -> "no"
